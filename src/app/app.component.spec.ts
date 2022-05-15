32766604e6a95106384901898ba19d1fb5da664f
Matthieu CZESKI <matthieu.czeski@gmail.com>
2022-05-15 21:01:50 +0000

    fixes some tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it('List number should contain random number between -1000 and 1000', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    const listNumber: number[] = app.generateRandomNumber();
+    const listNumber: number[] = app.generateRandomNumbers();
     expect(Math.max(...listNumber)).toBeLessThan(1000);
     expect(Math.min(...listNumber)).toBeGreaterThan(-1000);
   });
@@ -30,7 +30,7 @@ describe('AppComponent', () => {
     const listNumber = [3, 6, 9, 12];
     const listNumberManaged = listNumber.map(n => app.manageNumber(n));
     listNumberManaged.forEach(nManaged => {
-      expect(nManaged).toBe('Forced');
+      expect(nManaged).toBe('Geste');
     });
   });
 
@@ -50,7 +50,7 @@ describe('AppComponent', () => {
     const listNumber = [15, 30];
     const listNumberManaged = listNumber.map(n => app.manageNumber(n));
     listNumberManaged.forEach(nManaged => {
-      expect(nManaged).toBe('GestForme');
+      expect(nManaged).toBe('Gestform');
     });
   });
 
@@ -64,18 +64,19 @@ describe('AppComponent', () => {
     });
   });
 
-  it('manageNumber should return an error if send not a number', () => {
+  it('manageNumber should return an error if send not a integer', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    const expectAnErrorThere = app.manageNumber('Coucou :)');
-    expect(expectAnErrorThere).toThrowError('Expected a number');
+    expect(() => {
+      app.manageNumber('Coucou :)' as any);
+    }).toThrowError('Expected a number');
   });
 
-  it('Given [1,3,5,15,22] list number, Should return [1, "Geste", "Forme", "GesteForme", 22]', () => {
+  it('Given [1,3,5,15,22] list number, Should return [1, "Geste", "Form", "Gesteform", 22]', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     const givenListNumber = [1, 3, 5, 15, 22];
-    const expectedListNumber = givenListNumber.map(n => app.managedNumber(n));
-    expect(expectedListNumber).toBe([1, 'Geste', 'Forme', 'GesteForme', 22]);
+    const expectedListNumber = givenListNumber.map(n => app.manageNumber(n));
+    expect(expectedListNumber).toEqual([1, 'Geste', 'Forme', 'Gestform', 22]);
   });
 });
