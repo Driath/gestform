b841b9f7d4d4e3fff8c5dc35b2bbf024d2bc3b87
Matthieu CZESKI <matthieu.czeski@gmail.com>
2022-05-15 19:28:08 +0000

    Write specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,4 +15,67 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('List number should contain random number between -1000 and 1000', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const listNumber: number[] = app.generateRandomNumber();
+    expect(Math.max(...listNumber)).toBeLessThan(1000);
+    expect(Math.min(...listNumber)).toBeGreaterThan(-1000);
+  });
+
+  it('For a number N from the list number, if N is divisible per 3 return Geste', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const listNumber = [3, 6, 9, 12];
+    const listNumberManaged = listNumber.map(n => app.manageNumber(n));
+    listNumberManaged.forEach(nManaged => {
+      expect(nManaged).toBe('Forced');
+    });
+  });
+
+  it('For a number N from the list number, if N is divisible per 5 return Forme', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const listNumber = [5, 10]; // Omit voluntary 15 for next test :)
+    const listNumberManaged = listNumber.map(n => app.manageNumber(n));
+    listNumberManaged.forEach(nManaged => {
+      expect(nManaged).toBe('Forme');
+    });
+  });
+
+  it('For a number N from the list number, if N is divisible per 3 and per 5 return GestForme', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const listNumber = [15, 30];
+    const listNumberManaged = listNumber.map(n => app.manageNumber(n));
+    listNumberManaged.forEach(nManaged => {
+      expect(nManaged).toBe('GestForme');
+    });
+  });
+
+  it('For a number N from the list number, if N is not divisible per 3 or per 5 return N', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const listNumber = [1, 2, 4, 7];
+    const listNumberManaged = listNumber.map(n => app.manageNumber(n));
+    listNumberManaged.forEach((nManaged, i) => {
+      expect(nManaged).toBe(listNumber[i]);
+    });
+  });
+
+  it('manageNumber should return an error if send not a number', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const expectAnErrorThere = app.manageNumber('Coucou :)');
+    expect(expectAnErrorThere).toThrowError('Expected a number');
+  });
+
+  it('Given [1,3,5,15,22] list number, Should return [1, "Geste", "Forme", "GesteForme", 22]', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const givenListNumber = [1, 3, 5, 15, 22];
+    const expectedListNumber = givenListNumber.map(n => app.managedNumber(n));
+    expect(expectedListNumber).toBe([1, 'Geste', 'Forme', 'GesteForme', 22]);
+  });
 });
