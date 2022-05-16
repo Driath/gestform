85140ab3d6e2e9a385972e0cc456b1d55da11d6c
Matthieu CZESKI <matthieu.czeski@gmail.com>
2022-05-16 02:04:52 +0200

    trying tests with docker node:alpine

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,17 @@
 FROM node:alpine as build
 WORKDIR /app
 COPY . .
+
+
+RUN apk add python3 make g++
 RUN npm install
+
+# not working :( TODO: make it work later maybe found a solution from an image from ubuntu instead of minimalist alpine image
+# RUN apk add   chromium dbus chromium-chromedriver
+#ENV CHROME_BIN=/usr/bin/chromium-browser
+#RUN npx ng test --browsers=Headless_Chrome
+
+RUN npm run lint
 RUN npm run build
 
 # Stage run
--- karma.conf.js
+++ karma.conf.js
@@ -10,8 +10,20 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
+    customLaunchers: {
+      Headless_Chrome: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--headless',
+          '--disable-software-rasterizer',
+          '--disable-setuid-sandbox',
+        ],
+      },
+    },
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
@@ -19,18 +31,15 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/gestform'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -39,6 +48,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
